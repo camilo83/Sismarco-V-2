83611ebac13fb67e08e7ad77e3f004e9250872d5
camilo83 <camiloisazag@hotmail.com>
2025-01-23 14:13:51 -0500

    Add initial config

---
+++ src/store/store.ts
@@ -0,0 +1,31 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import eventsReducer from '../redux/eventsSlice';
+import worksReducer from '../redux/worksSlice';
+import artistsReducer from '../redux/artistsSlice';
+import worksPopUpPopUpReducer from '../redux/worksPopUpSlice';
+import artistsPopUpPopUpReducer from '../redux/artistsPopUpSlice';
+import menuReducer from '../redux/menuSlice';
+import productionsReducer from '../redux/productionsSlice';
+import mdeEventReducer from '../redux/mdeEventsSlice';
+
+export const store = configureStore({
+  reducer: {
+    eventsState: eventsReducer,
+    worksState: worksReducer,
+    artistsState: artistsReducer,
+    worksPopUpState: worksPopUpPopUpReducer,
+    artistsPopUpState: artistsPopUpPopUpReducer,
+    menuState: menuReducer,
+    productionsState: productionsReducer,
+    mdeEventState: mdeEventReducer,
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
